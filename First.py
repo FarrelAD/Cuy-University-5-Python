c959639e33760b7f25e954304c77d031edfc8c87
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-03-06 12:48:59 +0700

    add: first Python file

---
+++ First.py
@@ -0,0 +1,20 @@
+'''
+Mengikuti program kelas cepat 'CUY UNIVERSITY 5 - PYTHON' oleh Dea Afrizal
+Sumber: https://youtu.be/n0tURC_xeyI?si=ouX_DR2G-TIcxkiG
+'''
+
+# Dasar cetak tulisan
+print('This is my first Python code at "Cuy University" - 5')
+
+
+# Cetak multi baris dengan 3x petik satu
+print(
+    '''
+    Hallo bro!
+    
+    Saya adalah programmer handal!
+    Namaku adalah Farrel 
+    
+    SALAM KENAL!! :)
+    '''
+)
